e423efa8340a052baf80c45b5891905e3af29794
tamsir <tamsirababacardiouf@gmail.com>
2025-07-04 15:19:45 +0000

    new

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 // Configuration de la connexion avec pool pour optimiser les performances
 const dbConfig = {
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Tam@1#',
-  database: process.env.DB_NAME || 'bibliotheque_web',
-  port: process.env.DB_PORT || 4002,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
+  port: process.env.DB_PORT ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
